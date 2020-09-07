881e00e2a169d713fc8f0bfb8a5974370945de80
Dmitry Birin <dmitry.birin@gmail.com>
2020-09-07 11:10:07 +0300

    deleted the key from repo

--- machine.ts
+++ machine.ts
@@ -2,27 +2,31 @@ import { Machine, assign } from 'xstate'
 
 import Pusher from 'pusher-js/react-native'
 
+import { PUSHER_KEY } from './keys'
+
 Pusher.logToConsole = false
 
 const createPusher = async () => {
-    // try {
-        const pusher = new Pusher('04fc1f9a59013ef0cbd5', {
+    try {
+        if (!PUSHER_KEY) {
+            throw new Error('Oops, you need pusher key!')
+        }
+
+        const pusher = new Pusher(PUSHER_KEY, {
             cluster: 'eu',
         })
-        throw new Error('OOPS')
+
         return new Promise((res) =>
             pusher.connection.bind('connected', () => {
                 res(pusher)
                 pusher.connection.unbind_all()
             })
         )
-    // } catch (err) {
-    //     return new Promise((rej) => rej(err))
-    // }
-    
-    // await new Promise(res => setTimeout(res, 2000))
+    } catch (err) {
+        return new Promise((rej) => rej(err))
+    }
 
-    
+    // await new Promise(res => setTimeout(res, 2000))
 }
 
 export const pusheenMachine = Machine(
@@ -31,7 +35,7 @@ export const pusheenMachine = Machine(
         context: {
             pusher: null,
             channel: null,
-            lastError: null
+            lastError: null,
         },
         states: {
             idle: {
@@ -48,19 +52,19 @@ export const pusheenMachine = Machine(
                 },
             },
             offline: {},
-            failed: {}
+            failed: {},
         },
     },
     {
         actions: {
             setPusher: assign({
-                pusher: (ctx, event) => event.data
+                pusher: (ctx, event) => event.data,
             }),
             setChannel: assign({
-                channel: (ctx, event) => ctx.pusher?.subscribe('hal')
+                channel: (ctx, event) => ctx.pusher?.subscribe('hal'),
             }),
             setErrorMessage: assign({
-                lastError: (ctx, event) => event.data
+                lastError: (ctx, event) => event.data,
             }),
         },
     }
